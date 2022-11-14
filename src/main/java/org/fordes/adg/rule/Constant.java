80bb31128d84fe323a0e9a023358d3a0783d5100
fordes123 <ec.cheng.16888@gmail.com>
2022-11-14 10:28:07 +0800

    fix: 🐛 有效性校验与注释识别问题

--- src/main/java/org/fordes/adg/rule/Constant.java
+++ src/main/java/org/fordes/adg/rule/Constant.java
@@ -15,12 +15,12 @@ public class Constant {
     /**
      * 基本的有效性检测正则，!开头，[]包裹，非特殊标记的#号开头均视为无效规则
      */
-    public static final String EFFICIENT_REGEX = "^!|^#[^#,^@,^%,^\\$]|#\\s|^\\[.*\\]$";
+    public static final String EFFICIENT_REGEX = "^!|^#[^#,^@,^%,^\\$]|^\\[.*\\]$";
 
     /**
-     * 去除首尾基础修饰符号 的正则，方面对规则进行分类
+     * 去除首尾基础修饰符号 的正则，方便对规则进行分类
      * 包含：@@、||、@@||、/ 开头，$important、/ 结尾
      */
-    public static final String BASIC_MODIFY_REGEX = "^@@\\|\\||^\\|\\||^@@|\\$important$";
+    public static final String BASIC_MODIFY_REGEX = "^@@\\|\\||^\\|\\||^@@|\\$important$|\\s#[^#]*$";
 
 }
