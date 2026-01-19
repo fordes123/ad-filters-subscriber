996a2506b58a85af31dc1ec117a2e8c46d2afe52
fordes123 <52121395+fordes123@users.noreply.github.com>
2026-01-19 14:29:02 +0800

    feat: Add retry for HttpFetcher

--- src/main/java/org/fordes/adfs/handler/fetch/HttpFetcher.java
+++ src/main/java/org/fordes/adfs/handler/fetch/HttpFetcher.java
@@ -7,11 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.netty.http.client.HttpClient;
+import reactor.util.retry.Retry;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -22,25 +26,26 @@ import java.util.concurrent.TimeUnit;
 public class HttpFetcher extends Fetcher {
 
     private final WebClient webClient;
-    private Integer connectTimeout = 10_000;
-    private Integer readTimeout = 30_000;
-    private Integer writeTimeout = 30_000;
-    private Integer bufferSize = 4096;
+    private Duration connectTimeout = Duration.ofSeconds(10);
+    private Duration readTimeout = Duration.ofSeconds(30);
+    private Duration writeTimeout = Duration.ofSeconds(30);
+    private Duration responseTimeout = Duration.ofSeconds(30);
+    private DataSize bufferSize = DataSize.ofBytes(4096);
 
     public HttpFetcher() {
 
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, this.connectTimeout) // 连接超时 10 秒
-                .responseTimeout(Duration.ofSeconds(30))              // 响应超时 30 秒
+        final HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) this.connectTimeout.toMillis())
+                .responseTimeout(this.responseTimeout)
                 .doOnConnected(conn -> conn
-                        .addHandlerLast(new ReadTimeoutHandler(this.readTimeout, TimeUnit.MILLISECONDS))   // 读超时
-                        .addHandlerLast(new WriteTimeoutHandler(this.writeTimeout, TimeUnit.MILLISECONDS))  // 写超时
+                        .addHandlerLast(new ReadTimeoutHandler(this.readTimeout.toMillis(), TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(this.writeTimeout.toMillis(), TimeUnit.MILLISECONDS))
                 );
 
-        ExchangeStrategies strategies = ExchangeStrategies.builder()
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(configurer -> configurer
                         .defaultCodecs()
-                        .maxInMemorySize(this.bufferSize)
+                        .maxInMemorySize((int) this.bufferSize.toBytes())
                 )
                 .build();
 
@@ -53,10 +58,12 @@ public class HttpFetcher extends Fetcher {
                 .build();
     }
 
-    public HttpFetcher(Integer connectTimeout, Integer readTimeout, Integer writeTimeout, Integer bufferSize) {
+    public HttpFetcher(Duration connectTimeout, Duration readTimeout, Duration writeTimeout,
+                       Duration responseTimeout, DataSize bufferSize) {
         this();
         this.connectTimeout = connectTimeout;
         this.readTimeout = readTimeout;
+        this.responseTimeout = responseTimeout;
         this.writeTimeout = writeTimeout;
         this.bufferSize = bufferSize;
     }
@@ -67,6 +74,17 @@ public class HttpFetcher extends Fetcher {
                 .uri(URI.create(path))
                 .retrieve()
                 .bodyToFlux(DataBuffer.class)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1))
+                        .filter(t -> {
+                            if (t instanceof IOException) return true;
+                            if (t instanceof WebClientResponseException e) {
+                                return e.getStatusCode().is5xxServerError();
+                            }
+                            return false;
+                        })
+                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
+                            return retrySignal.failure();
+                        }))
                 .onErrorResume(e -> {
                     log.error("http rule => {}, fetch failed  => {}", path, e.getMessage(), e);
                     return Flux.empty();
